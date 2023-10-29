fbcd33873a3aa38de143cbe12a87c91c6185f80f
2007 2007 <E2007@365i.team>
2023-10-29 19:30:44 +0100

    alternating caps

--- CounterAdvanced/Program.cs
+++ CounterAdvanced/Program.cs
@@ -3,89 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 
 namespace CounterAdvanced
 {
-    internal class ZliczLitery
+    internal class zLiCzLiTeRy
     {
-        static Dictionary<char, int> CalculateCharFrequency(string inputText)
+        static string WczytajTekst()
         {
-        Dictionary<char, int> charFrequency = new Dictionary<char, int>();
+            Console.WriteLine("Wybierz źródło tekstu:");
+            Console.WriteLine("1. Wczytaj tekst z klawiatury");
+            Console.WriteLine("2. Wczytaj tekst z uRla");
 
-        foreach (char c in inputText)
-        {
-            if (Char.IsLetter(c))
+            int cHoIcE;
+            while (!int.TryParse(Console.ReadLine(), out cHoIcE) || (cHoIcE != 1 && cHoIcE != 2))
             {
-                char lowercaseChar = Char.ToLower(c);
-                if (charFrequency.ContainsKey(lowercaseChar))
+                Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+            }
+
+            if (cHoIcE == 1)
+            {
+                Console.WriteLine("Podaj tekst:");
+                return Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Podaj adres uRl:");
+                string uRl = Console.ReadLine();
+                try
                 {
-                    charFrequency[lowercaseChar]++;
+                    using (WebClient cLiEnT = new WebClient())
+                    {
+                        return cLiEnT.DownloadString(uRl);
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    charFrequency[lowercaseChar] = 1;
+                    Console.WriteLine($"Wystąpił błąd podczas pobierania tekstu: {ex.Message}");
+                    return null;
                 }
             }
         }
 
-        return charFrequency;
+        static Dictionary<char, int> CalculatecHaRfReQuEnCy(string iNpUtTeXt, List<char> lEtTeRsToChEcK)
+        {
+            Dictionary<char, int> cHaRfReQuEnCy = new Dictionary<char, int>();
+
+            foreach (char c in iNpUtTeXt)
+            {
+                if (Char.IsLetter(c) && lEtTeRsToChEcK.Contains(c))
+                {
+                    char lOwErCaSeChAr = Char.ToLower(c);
+                    if (cHaRfReQuEnCy.ContainsKey(lOwErCaSeChAr))
+                    {
+                        cHaRfReQuEnCy[lOwErCaSeChAr]++;
+                    }
+                    else
+                    {
+                        cHaRfReQuEnCy[lOwErCaSeChAr] = 1;
+                    }
+                }
+            }
+
+            return cHaRfReQuEnCy;
         }
 
-        static void DisplayHistogram(Dictionary<char, int> charFrequency)
+        static void DisplayHistogram(Dictionary<char, int> cHaRfReQuEnCy)
         {
             Console.WriteLine("Histogram częstości występowania liter:");
 
-            foreach (var entry in charFrequency)
+            foreach (var eNtRy in cHaRfReQuEnCy)
             {
-            Console.WriteLine($"{entry.Key}: {new string('*', entry.Value)}");
+                Console.WriteLine($"{eNtRy.Key}: {new string('*', eNtRy.Value)}");
             }
         }
-         
-        static void SaveHistogramToFile(string filePath, Dictionary<char, int> charFrequency)
-        {
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                writer.WriteLine("Histogram częstości występowania liter:");
 
-                foreach (var entry in charFrequency)
+        static void SaveHistogramToFile(string fIlEpAtH, Dictionary<char, int> cHaRfReQuEnCy)
+        {
+            using (StreamWriter wRiTeR = new StreamWriter(fIlEpAtH))
+            {
+                wRiTeR.WriteLine("Histogram częstości występowania liter:");
+
+                foreach (var eNtRy in cHaRfReQuEnCy)
                 {
-                    writer.WriteLine($"{entry.Key}: {new string('*', entry.Value)}");
+                    wRiTeR.WriteLine($"{eNtRy.Key}: {new string('*', eNtRy.Value)}");
                 }
             }
         }
 
-        //public static int Zlicz(string napis)
-        //{
-        //    int suma = 0;
-        //   suma = napis.Length;
-        //    return suma;
-        //}
-        // public static char PierwszyZnak(string napis)
-        //{
-        //    return napis[0];
-        //}
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj tekst:");
-            string inputText = Console.ReadLine();
+            string iNpUtTeXt = WczytajTekst();
 
-            Dictionary<char, int> charFrequency = CalculateCharFrequency(inputText);
-            DisplayHistogram(charFrequency);
-
-            //Console.WriteLine("Podaj ścieżkę do pliku, w którym chcesz zapisać histogram:");
-            string filePath = "C:\\Users\\kamil\\Desktop\\Wynik";
-
-            try
+            if (iNpUtTeXt != null)
             {
-                SaveHistogramToFile(filePath, charFrequency);
-                Console.WriteLine($"Histogram został zapisany do pliku: {filePath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Wystąpił błąd podczas zapisywania pliku: {ex.Message}");
+                Console.WriteLine("Podaj zestaw liter do sprawdzenia (oddzielone przecinkami, np. a,b,c):");
+                string lEtTeRsInPuT = Console.ReadLine();
+
+                string[] lEtTeRsArray = lEtTeRsInPuT.Split(',');
+                List<char> lEtTeRsToChEcK = new List<char>();
+                foreach (string lEtTeR in lEtTeRsArray)
+                {
+                    char singlelEtTeR = Convert.ToChar(lEtTeR.Trim());
+                    lEtTeRsToChEcK.Add(singlelEtTeR);
+                }
+
+                Dictionary<char, int> cHaRfReQuEnCy = CalculatecHaRfReQuEnCy(iNpUtTeXt, lEtTeRsToChEcK);
+                DisplayHistogram(cHaRfReQuEnCy);
+
+                string fIlEpAtH = "C:\\Users\\kamil\\Desktop\\Wynik\\wynik.txt";
+
+                try
+                {
+                    SaveHistogramToFile(fIlEpAtH, cHaRfReQuEnCy);
+                    Console.WriteLine($"Histogram został zapisany do pliku: {fIlEpAtH}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Wystąpił błąd podczas zapisywania pliku: {ex.Message}");
+                }
             }
 
             Console.ReadKey();
